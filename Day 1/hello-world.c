8f0f6bdaa7f824fed355d14ea73a5e7b53148016
DasIschBims <dasischbims@gmail.com>
2023-01-11 18:55:18 +0100

    how could i forget the classic hello world?

---
+++ Day 1/hello-world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// I didn't really think anyone would check my stuff out but since you're here, have a nice day!
+
+int main(void)
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
